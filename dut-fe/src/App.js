affbd81c90a03c872634e803fb2fe91ae26de019
Oluwatobiloba Olalusi <o.olalusi@esar-gmbh.de>
2019-11-24 09:45:58 +0100

    frontend before hackathon

--- dut-fe/src/App.js
+++ dut-fe/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import CountUp from "react-countup";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 import "./App.css";
 import model1 from "./model1.png";
 import model2 from "./model2.png";
 import {
-	DecorativeAxis,
+	ContinuousColorLegend,
 	HeatmapSeries,
 	LineSeries,
 	XAxis,
@@ -16,32 +18,21 @@ import {
 import demo0 from "./demo0";
 import demo1 from "./demo1";
 
-const heatmap = [
-	{ x: 1, y: 0 },
-	{ x: 1, y: 5 },
-	{ x: 1, y: 10 },
-	{ x: 1, y: 15 },
-	{ x: 2, y: 0 },
-	{ x: 2, y: 5 },
-	{ x: 2, y: 10 },
-	{ x: 2, y: 15 },
-	{ x: 3, y: 0 },
-	{ x: 3, y: 5 },
-	{ x: 3, y: 10 },
-	{ x: 3, y: 15 }
-];
-
 const App = () => {
 	const [demoId, setDemoId] = useState(0);
-	const [graph, setGraph] = useState("covariance");
-	const [covr, setCovr] = useState([]);
+	const [graph, setGraph] = useState("correlation");
+	const [corr, setCorr] = useState([]);
 	const [pof, setPof] = useState([]);
 	const [poft, setPoft] = useState([]);
+	const [modelImage, setModelImage] = useState(0);
+	const [modelImageIsOpen, setModelImageIsOpen] = useState(false);
 
 	const demos = [demo0, demo1];
 
+	const modelImages = [model1, model2];
+
 	useEffect(() => {
-		setCovr(convertCovr());
+		setCorr(convertCorr());
 		setPof(convertPoF());
 		setPoft(convertPoFT());
 	}, [demoId]);
@@ -57,20 +48,20 @@ const App = () => {
 	};
 
 	// Convert covariance to vis data
-	const convertCovr = () => {
-		const covr = [];
+	const convertCorr = () => {
+		const corr = [];
 		let idx = 0;
-		for (let i = 0; i < demos[demoId].covr_mtrx.length; i++) {
-			for (let j = 0; j < demos[demoId].covr_mtrx[i].length; j++) {
-				covr[idx] = {
+		for (let i = 0; i < demos[demoId].corr_mtrx.length; i++) {
+			for (let j = 0; j < demos[demoId].corr_mtrx[i].length; j++) {
+				corr[idx] = {
 					x: i,
 					y: j,
-					color: Math.round(demos[demoId].covr_mtrx[i][j] * 10)
+					color: Math.round(demos[demoId].corr_mtrx[i][j] * 10)
 				};
 				idx++;
 			}
 		}
-		return covr;
+		return corr;
 	};
 
 	// Convert pass or fail to vis data
@@ -151,7 +142,7 @@ const App = () => {
 							(1 -
 								demos[demoId].failed_devices /
 									demos[demoId].total_devices) *
-							100
+							99
 						}
 						suffix="%"
 						delay={0.4}
@@ -163,24 +154,26 @@ const App = () => {
 					<ul>
 						<li
 							className={`${
-								graph === "covariance"
+								graph === "correlation"
 									? "bg-blue-5 text-gray-400"
 									: "bg-blue-3 text-gray-700"
 							} p-4 my-4 cursor-pointer hover:bg-blue-5 hover:text-gray-400`}
-							onClick={() => handleGraphChange("covariance")}
+							onClick={() => handleGraphChange("correlation")}
 						>
-							Covariance matrix
+							Correlation matrix
 						</li>
-						<li
-							className={`${
-								graph === "model_details"
-									? "bg-blue-5 text-gray-400"
-									: "bg-blue-3 text-gray-700"
-							} p-4 my-4 cursor-pointer hover:bg-blue-5 hover:text-gray-400`}
-							onClick={() => handleGraphChange("model_details")}
-						>
-							Model details
-						</li>
+						{demoId !== 0 && (
+							<li
+								className={`${
+									graph === "model_details"
+										? "bg-blue-5 text-gray-400"
+										: "bg-blue-3 text-gray-700"
+								} p-4 my-4 cursor-pointer hover:bg-blue-5 hover:text-gray-400`}
+								onClick={() => handleGraphChange("model_details")}
+							>
+								Model details
+							</li>
+						)}
 						<li
 							className={`${
 								graph === "pass_or_fail"
@@ -199,11 +192,8 @@ const App = () => {
 							onChange={() => handleDemoSelect()}
 							defaultValue={demoId}
 						>
-							<option value={0}>Demo 0</option>
-							<option value={1}>Demo 1</option>
-							<option value={2}>Demo 2</option>
-							<option value={3}>Demo 3</option>
-							<option value={4}>Demo 4</option>
+							<option value={0}>Default</option>
+							<option value={1}>Augmented data</option>
 						</select>
 						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
 							<svg
@@ -230,40 +220,76 @@ const App = () => {
 						</div>
 					</div>
 				</menu>
-				{graph === "covariance" && (
+				{graph === "correlation" && (
 					<div className="mx-auto flex justify-center">
 						<XYPlot
 							className="mx-2 my-4"
 							width={450}
 							height={450}
-							yDomain={[demos[demoId].covr_mtrx.length, 0]}
+							colorRange={["#FF4602", "#FDFF29"]}
+							yDomain={[demos[demoId].corr_mtrx.length, 0]}
 						>
 							<XAxis />
 							<YAxis />
-							<HeatmapSeries data={covr} />
+							<HeatmapSeries data={corr} />
+							<ContinuousColorLegend
+								startTitle="0.0"
+								midTitle="0.5"
+								endTitle="1.0"
+								startColor="#FF4602"
+								endColor="#FDFF29"
+								height={10}
+							/>
 						</XYPlot>
 					</div>
 				)}
 				{graph === "model_details" && (
 					<div className="mx-auto flex justify-center">
-						<img
-							style={{ height: 500 }}
-							src={model1}
-							alt="Model 1 image"
-						/>
-						<img
-							style={{ height: 500 }}
-							src={model2}
-							alt="Model 2 image"
-						/>
+						<div className="bg-white p-4 mx-2">
+							<img
+								onClick={() => {
+									setModelImage(0);
+									setModelImageIsOpen(true);
+								}}
+								style={{ height: 500 }}
+								src={model1}
+							/>
+						</div>
+						<div className="bg-white p-4 mx-2">
+							<img
+								onClick={() => {
+									setModelImage(1);
+									setModelImageIsOpen(true);
+								}}
+								style={{ height: 500 }}
+								src={model2}
+							/>
+						</div>
+						{modelImageIsOpen && (
+							<Lightbox
+								mainSrc={modelImages[modelImage]}
+								wrapperClassName="bg-white p-4"
+								onCloseRequest={() => setModelImageIsOpen(false)}
+							/>
+						)}
 					</div>
 				)}
 				{graph === "pass_or_fail" && (
 					<div className="mx-auto flex justify-center">
 						<XYPlot className="mx-2 my-4" width={900} height={450}>
-							<XAxis style={{ line: { stroke: "#00214A" } }} />
-							<YAxis style={{ line: { stroke: "#00214A" } }} />
-							<LineSeries color="#F16329" strokeWidth={1} data={pof} />
+							<XAxis
+								style={{
+									line: { stroke: "#00214A" },
+									text: { fill: "white" }
+								}}
+							/>
+							<YAxis
+								style={{
+									line: { stroke: "#00214A" },
+									text: { fill: "white" }
+								}}
+							/>
+							<LineSeries color="#FF4602" strokeWidth={1} data={pof} />
 							<LineSeries color="white" data={poft} />
 						</XYPlot>
 					</div>
